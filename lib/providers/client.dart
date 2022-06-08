de25f3de0bac70a8fdcf4173f36d54e8181ccf03
Bishwajeet Parhi <62933155+2002Bishwajeet@users.noreply.github.com>
2022-06-08 16:45:29 +0530

    update todo info

--- lib/providers/client.dart
+++ lib/providers/client.dart
@@ -1,7 +1,7 @@
 import 'package:appwrite/appwrite.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-// TODO: Replace with proper info
+// TODO: Replace with your endpoint and project Id
 final clientProvider = Provider<Client>((ref) {
   return Client()
       .setEndpoint('[YOUR_ENDPOINT]')
